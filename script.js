41d5418915f853b9b38a870181e2f50e6f3ecb77
Lucas Vieira Souza <127160174+LucasVieiraaaa@users.noreply.github.com>
2024-02-21 01:52:46 -0300

    first commit
    21-02

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("enviarEmail").addEventListener("click", function() {
+    window.location.href = "mailto:lucasvieirasouzaa@gmail.com?subject=Hi%20Lucas%20Souza,%20I%20saw%20your%20portfolio%20and%20would%20like%20to%20get%20in%20touch!";
+  });
