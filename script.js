788ca7af6ea99877b8203f8ba939a8125582c705
Lucas <lucasvieirasouzaa@gmail.com>
2024-02-21 14:59:42 -0300

    Update to Portifolio 1.2

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
+  // Function that sends me messages via Gmail
+
 document.getElementById("enviarEmail").addEventListener("click", function() {
     window.location.href = "mailto:lucasvieirasouzaa@gmail.com?subject=Hi%20Lucas%20Souza,%20I%20saw%20your%20portfolio%20and%20would%20like%20to%20get%20in%20touch!";
   });
 
+
+  // Function that sends me messages via WhatsApp
   function WhatsApp() {
     var x = "5531975580097";
     var mensagem = "Hi Lucas Souza, I saw your portfolio and would like to get in touch!";
-
-    // Formato do link para o WhatsApp
     var link = "https://api.whatsapp.com/send?phone=" + x + "&text=" + encodeURIComponent(mensagem);
-
-    // Abrir link no WhatsApp
     window.open(link);
 }
