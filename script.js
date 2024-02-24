310600793cd5a81e78c90b1194554952e2a310b0
Lucas <lucasvieirasouzaa@gmail.com>
2024-02-24 10:48:30 -0300

    Fixing bugs

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ document.getElementById("enviarEmail").addEventListener("click", function() {
     window.location.href = "mailto:lucasvieirasouzaa@gmail.com?subject=Hi%20Lucas%20Souza,%20I%20saw%20your%20portfolio%20and%20would%20like%20to%20get%20in%20touch!";
   });
 
+function enviarEmail01(){
+    window.location.href = "mailto:lucasvieirasouzaa@gmail.com?subject=Hi%20Lucas%20Souza,%20I%20saw%20your%20portfolio%20and%20would%20like%20to%20get%20in%20touch!";
+  };
+
 
   // Function that sends me messages via WhatsApp
   function WhatsApp() {
