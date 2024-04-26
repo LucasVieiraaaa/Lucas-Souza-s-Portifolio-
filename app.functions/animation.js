ca1029eac540c67a6432b5f3106d12395633b838
Lucas <lucasvieirasouzaa@gmail.com>
2024-04-26 19:44:45 -0300

    Updating all archives, Portifolio v1.6

---
+++ app.functions/animation.js
@@ -0,0 +1,17 @@
+  // Fade-In animation
+  document.addEventListener("DOMContentLoaded", function() {
+    var fadeContent = document.getElementById('fade-in-content');
+    fadeContent.classList.add('fade-in-active');
+  });
+  
+  
+  window.addEventListener('scroll', function() {
+    var textBox = document.querySelector('.slide_animation');
+    var rect = textBox.getBoundingClientRect();
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  
+    // Se o text-box estiver na visão do usuário
+    if (rect.top < windowHeight && rect.bottom >= 0) {
+      textBox.style.opacity = '1'; // Define a opacidade para 1 para iniciar a animação
+    }
+  });
