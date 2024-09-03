f47aae81c6ef8518f846fdcbe65287c3af594337
unknown <lucasvieirasouzaa@gmail.com>
2024-09-02 22:03:33 -0300

    Update

--- app.functions/animation.js
+++ app.functions/animation.js
@@ -9,13 +9,13 @@
 });
   
   
-  window.addEventListener('scroll', function() {
-    var textBox = document.querySelector('.slide_animation');
-    var rect = textBox.getBoundingClientRect();
-    var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  // window.addEventListener('scroll', function() {
+  //   var textBox = document.querySelector('.slide_animation');
+  //   var rect = textBox.getBoundingClientRect();
+  //   var windowHeight = window.innerHeight || document.documentElement.clientHeight;
   
-    // Se o text-box estiver na visão do usuário
-    if (rect.top < windowHeight && rect.bottom >= 0) {
-      textBox.style.opacity = '1'; // Define a opacidade para 1 para iniciar a animação
-    }
-  });
+  //   // Se o text-box estiver na visão do usuário
+  //   if (rect.top < windowHeight && rect.bottom >= 0) {
+  //     textBox.style.opacity = '1'; // Define a opacidade para 1 para iniciar a animação
+  //   }
+  // });
