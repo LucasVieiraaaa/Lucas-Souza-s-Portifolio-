daac0284739c17460fbbbcb58f1624eee17a1f0e
unknown <lucasvieirasouzaa@gmail.com>
2024-10-03 23:07:31 -0300

    Adding en.js in index.html

---
+++ app.functions/en.js
@@ -0,0 +1,39 @@
+let languages = "Languages";
+document.getElementById('languages').textContent = languages;
+
+let Home = "Home";
+document.getElementById('home').textContent = Home;
+
+let resume = "Resume";
+document.getElementById('resume').textContent = resume;
+
+let english = 'English - Fluent';
+document.getElementById('english').textContent = english;
+
+let france = 'French - Basic';
+document.getElementById('french').textContent = france;
+
+let spanish = 'Spanish - Inter';
+document.getElementById('spanish').textContent = spanish;
+
+let letsBeSocial = 'Let\'\s be social';
+document.getElementById('letsBeSocial').textContent = letsBeSocial;
+
+let letsChat = 'Let\'\s Chat';
+document.getElementById('letsChat').textContent = letsChat;
+
+let resumeUpdatedDate = '09/02/2024'
+let resumeUpdated = `Resume Updated in ${resumeUpdatedDate}`;
+document.getElementById('resumeUpdated').textContent = resumeUpdated;
+
+let frontEnd = 'FRONT-END';
+document.getElementById('frontEnd').textContent = frontEnd;
+
+let backEnd = 'BACK-END';
+document.getElementById('backEnd').textContent = frontEnd;
+
+let infrastructure = 'INFRASTRUCTURE';
+document.getElementById('infrastructure').textContent = infrastructure;
+
+let fullStack = 'FULL STACK';
+document.getElementById('fullStack').textContent = fullStack;
